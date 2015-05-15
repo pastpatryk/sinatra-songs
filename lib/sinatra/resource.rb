ae22ed56a91c9ccd971891595629dcfc8b2400a1
Patryk Pastewski <past.patryk@gmail.com>
2015-05-15 22:00:53 +0200

    added regitered method

--- lib/sinatra/resource.rb
+++ lib/sinatra/resource.rb
@@ -2,6 +2,19 @@ module Sinatra
   module Restful
     module Resource
 
+      def self.extended
+        base.instance_variable_set :@endpoints, []
+      end
+
+      def registered(app)
+        @endpoints.each do |e|
+          app.send(e[:type], e[:route], &e[:block])
+        end
+      end
+
+      def get(route, &action)
+        @endpoints << {type: :get, route: route, action: action}
+  		end
 
     end
   end
